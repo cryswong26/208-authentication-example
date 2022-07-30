9cc3376a8fc1f3d59f5bac58b0ca5021a7139808
Crystal <cryswong26@gmail.com>
2022-07-30 00:15:17 +0000

    changed colors

--- app.py
+++ app.py
@@ -45,9 +45,7 @@ def update_graph(dropdown_value):
 
     x_values = [-3,-2,-1,0,1,2,3]
     y_values = [(x**dropdown_value)**2 for x in x_values]
-    #y_values = [(x**2-x**4)**.5 for x in x_values]
-    #y_values = [x*math.sin(1/x) for x in x_values]
-    colors=['black','red','green','blue','orange','purple']
+    colors=['black','pink','pink','pink','pink','pink']
     graph_title='Graph of {}'.format(str(dropdown_value))
 
 
